a383de7fdd56901bb296f1cc992eff20dd26ee20
Kerilom (Glebun08) <89039577+Gleb-Lisovskiy@users.noreply.github.com>
2026-09-07 17:17:05 +0400

    Updated IScriptManagedSound

--- src/main/java/mchorse/mappet/client/ClientTriggers.java
+++ src/main/java/mchorse/mappet/client/ClientTriggers.java
@@ -10,13 +10,6 @@ public final class ClientTriggers {
     private ClientTriggers() {
     }
 
-    public static void trigger(String name, DataContext context) {
-        if (name == null || name.isEmpty() || context == null) {
-            return;
-        }
-
-        if (MappetClient.clientSettings != null) {
-            MappetClient.clientSettings.trigger(name, context);
         }
     }
 }
